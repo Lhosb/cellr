5a8e30c74f8455b65927b8ac3348fabd9b937529
Luke Olson <lhosb95@mac.com>
2026-03-07 17:05:43 -0800

    feat: enhance profile management with editable fields and cellar invitations

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,17 +1,40 @@
 class ProfilesController < ApplicationController
   def show
     @user = current_user
+    @cellars = Cellar.left_outer_joins(:cellar_memberships)
+                     .where("cellars.owner_id = :user_id OR cellar_memberships.user_id = :user_id", user_id: current_user.id)
+                     .includes(:owner)
+                     .order(created_at: :desc)
+
+    @pending_invitations = current_user.pending_invitations.includes(:cellar, :invited_by).order(created_at: :desc)
+    @editing = params[:edit].present?
   end
 
   def update
+    @user = current_user
+
     ActiveRecord::Base.transaction do
-      current_user.update!(profile_params)
+      if requires_current_password?(profile_params)
+        unless @user.valid_password?(profile_params[:current_password].to_s)
+          @user.errors.add(:current_password, "is incorrect")
+          raise ActiveRecord::RecordInvalid.new(@user)
+        end
+      end
+
+      update_attrs = profile_params.except(:current_password)
+
+      # Remove blank password params so Devise doesn't treat them as attempted updates
+      if update_attrs[:password].blank?
+        update_attrs.delete(:password)
+        update_attrs.delete(:password_confirmation)
+      end
+      @user.update!(update_attrs)
 
       case params[:drinking_status]
       when "start"
-        DrinkingSessions::Start.call(user: current_user)
+        DrinkingSessions::Start.call(user: @user)
       when "stop"
-        DrinkingSessions::Stop.call(user: current_user)
+        DrinkingSessions::Stop.call(user: @user)
       end
     end
 
@@ -24,6 +47,12 @@ class ProfilesController < ApplicationController
   private
 
   def profile_params
-    params.require(:user).permit(:name)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+  end
+
+  def requires_current_password?(attrs)
+    return true if attrs[:password].present?
+    return false unless attrs[:email].present?
+    attrs[:email].to_s.downcase != current_user.email.to_s.downcase
   end
 end
