333bf8f88f9c6af0a8b88645b6ca9fc418ef439b
Luke Olson <lolson@finetune.com>
2026-03-13 17:22:01 -0700

    fix: Correct joins in accessible_wines method for cellar associations

--- app/controllers/happy_hour_controller.rb
+++ app/controllers/happy_hour_controller.rb
@@ -8,8 +8,8 @@ class HappyHourController < ApplicationController
   private
 
   def accessible_wines
-    Wine.joins(:cellar)
-      .left_outer_joins(cellar: :cellar_memberships)
+    Wine.joins(:cellars)
+      .left_outer_joins(cellars: :cellar_memberships)
       .where("cellars.owner_id = :user_id OR cellar_memberships.user_id = :user_id", user_id: current_user.id)
       .includes(:winery, :region_record)
       .distinct
